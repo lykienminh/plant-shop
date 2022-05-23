34b3fda398432f88f56c4371b87af082e05bf3e8
Ly Kien Minh <lykienminh9@gmail.com>
2022-05-24 04:00:35 +0700

    remove service item

--- front-end/src/components/listServices/listServices.js
+++ front-end/src/components/listServices/listServices.js
@@ -18,18 +18,18 @@ const listServices = [
       description: "Các mẹo nhỏ giúp bạn xây dựng không gian xanh ngay trong chính căn nhà của mình",
       image: 'https://cdn.shopify.com/s/files/1/0560/7145/4754/articles/blog-1_1060x.jpg?v=1643965268',
     },
-    {
-      id: 4,
-      name: "Một ngày chăm sóc cây",
-      description: "Không có ai muốn khổ đau cho chính mình, muốn tìm kiếm về nó và muốn có nó, bởi vì nó là sự đau khổ...",
-      image: 'https://picsum.photos/593/679',
-    },
-    {
-      id: 5,
-      name: "Lorem ipsum dolor sit amet",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla consequat nulla ut lobortis fringilla. Pellentesque dapibus rhoncus diam, nec pharetra neque mattis sed. Etiam egestas tempus vehicula. Sed finibus molestie orci vulputate efficitur. Nulla porttitor pellentesque est ac tincidunt. Cras scelerisque mauris at condimentum cursus. Sed dapibus leo eu dapibus.",
-      image: 'https://picsum.photos/575/658',
-    },
+    // {
+    //   id: 4,
+    //   name: "Một ngày chăm sóc cây",
+    //   description: "Không có ai muốn khổ đau cho chính mình, muốn tìm kiếm về nó và muốn có nó, bởi vì nó là sự đau khổ...",
+    //   image: 'https://picsum.photos/593/679',
+    // },
+    // {
+    //   id: 5,
+    //   name: "Lorem ipsum dolor sit amet",
+    //   description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla consequat nulla ut lobortis fringilla. Pellentesque dapibus rhoncus diam, nec pharetra neque mattis sed. Etiam egestas tempus vehicula. Sed finibus molestie orci vulputate efficitur. Nulla porttitor pellentesque est ac tincidunt. Cras scelerisque mauris at condimentum cursus. Sed dapibus leo eu dapibus.",
+    //   image: 'https://picsum.photos/575/658',
+    // },
 ];
 
 export default listServices;
